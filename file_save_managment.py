8f873eed1594e5f1ac3faee2892e1e5f82f54f2d
vbenz <vincentbenz4@gmail.com>
2024-12-18 11:02:14 +0100

    Changed some hdf stuff

--- file_save_managment.py
+++ file_save_managment.py
@@ -11,8 +11,9 @@ def create_new_hdf5(file_path:str):
 
 def create_hdf5_group(folder_path:str, group_name:str):
     with h5py.File(folder_path,"w") as f:
-        f.create_group(group_name)
+        grp = f.create_group(group_name)
     print("Created new group:", group_name, "at:", folder_path)
+    return grp
 
 def add_data_to_hdf(path:str, data, dataset_name:str):
     with h5py.File(path, "w") as f:
@@ -28,8 +29,18 @@ def add_plot_to_hdf(file_path:str, plot_path:str, plot_name:str):
 
 
 if __name__ == "__main__":
-    hdf_file="test"
-    create_new_hdf5(hdf_file)
+    #hdf_file="test"
+    #create_new_hdf5(hdf_file)
+    #grp = create_hdf5_group(hdf_file, "test_group")
+    #print(grp.name)
+    #test_data=np.array([1,2,3,4,5])
+    #add_data_to_hdf(hdf_file, test_data, "test_data")
 
+    with h5py.File("mytestfile.hdf5", "a") as f:
+        grp = f.create_group("subgroup2")
+        print(list(f.keys()))
+        #dset = f.create_dataset("mydataset", (100,), dtype='i')
+        f.flush()
+    #print(dset.name)
 
 
